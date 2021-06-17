74561f303bc8667bfb54239f64310a703b72946a
Austin Reilly <areilly@alphaworks.tech>
2021-06-16 21:45:56 -0700

    changed to an array, need to work on the question checking system

--- script/magic8ball.js
+++ script/magic8ball.js
@@ -1,19 +1,3 @@
-function ask()
-{
-    let question = document.getElementById("question").value;
-
-    let randNumber = Math.floor(Math.random() * 31);
-
-    
-    pickAnswer(randNumber, question);
-    
-    randNumber = 10;
-    
-    document.getElementById("answer").innerHTML = `${pickAnswer(randNumber, question)}`;
-
-    console.log(randNumber);
-}
-
 function checkQuestion(num, question)
 {
     if (num === 4)
@@ -22,7 +6,10 @@ function checkQuestion(num, question)
     }
     if (num === 10)
     {
-        return checkQuestion(num - Math.floor(Math.random() * 10) + 2, question);
+        let redo = Math.floor(Math.random() * 31);
+
+
+        return num - Math.floor(Math.random() * 9);
     }
     if (question === "What's your favorite color?")
     {
@@ -34,75 +21,50 @@ function checkQuestion(num, question)
     }
 }
 
-function pickAnswer(finalNum, question)
+function ask()
 {
-    console.log(finalNum);
+    let question = document.getElementById("question").value;
 
-    switch (checkQuestion(finalNum, question))
-    {
-        case 0:
-            return "This is a sign. A bad sign.";
-        case 1:
-            return "Can you rephrase that?";
-        case 2:
-            return "There are is more than 1";
-        case 3:
-            return "Ask three more times";
-        case 4:
-            return "Blue";
-        case 5:
-            return "Purple";
-        case 6:
-            return "Red";
-        case 7:
-            return "What is your favorite color?";
-        case 8:
-            return "Toyota";
-        case 9:
-            return "BMW";
-        case 10:
-            return "Don't Blink.";
-        case 11:
-            return "I'm behind you";
-        case 12:
-            return "Just kidding xD";
-        case 13:
-            return "It's under your keyboard";
-        case 14:
-            return "Take a deep breath";
-        case 15:
-            return "Ask again later";
-        case 16:
-            return "Yes";
-        case 17:
-            return "No";
-        case 18:
-            return "In the fridge";
-        case 19:
-            return "I am hungry";
-        case 20:
-            return "Don't google that.";
-        case 21:
-            return "You're about to drop your phone";
-        case 22:
-            return "Omg are you serious?";
-        case 23:
-            return "Leave town.";
-        case 24:
-            return "CPP is better than JS";
-        case 25:
-            return "Watch out for bears";
-        case 26:
-            return "Bigfoot is real";
-        case 27:
-            return "That is not a good friend";
-        case 28:
-            return "That is a good friend";
-        case 29:
-            return "Send it";
-        case 30:
-            return "FUCKING SEND IT!!!";
-        default:
-            return "error";
-    }
+    let randNumber = Math.floor(Math.random() * 31);
+    
+    randNumber = 10;
+
+    let checkedNumber = checkQuestion(randNumber, question);
+    
+    console.log(checkedNumber);
+    
+    document.getElementById("answer").innerHTML = `${answers[checkedNumber]}`;
 }
+
+const answers = ["This is a sign. A bad sign.",
+"Can you rephrase that?",
+"There are is more than 1",
+"Ask three more times",
+"Blue",
+"Purple",
+"Red",
+"What is your favorite color?",
+"Toyota",
+"BMW",
+"Don't Blink.",
+"I'm behind you",
+"Just kidding xD",
+"It's under your keyboard",
+"Take a deep breath",
+"Ask again later",
+"Yes",
+"No",
+"In the fridge",
+"I am hungry",
+"Don't google that.",
+"You're about to drop your phone",
+"Omg are you serious?",
+"Leave town.",
+"CPP is better than JS",
+"Watch out for bears",
+"Bigfoot is real",
+"That is not a good friend",
+"That is a good friend",
+"Send it",
+"FUCKING SEND IT!!!",
+"error"]
