93fbb2cd6d84e44d9bf5bd8b1d0f26302c018ee1
Austin Reilly <areilly@alphaworks.tech>
2021-06-21 19:51:23 -0700

    put the answers back in the magic8ball script, added a couple hardCoded answers

--- script/magic8ball.js
+++ script/magic8ball.js
@@ -13,13 +13,54 @@ function ask()
 
 function isHardCoded(question)
 {
-    if (question === "What is your favorite color?")
+    switch (question)
     {
-        document.getElementById("answer").innerHTML = "Blue";
-        return true;
-    }
-    else
-    {
-        return false;
+        case "What is your favorite color?":
+            document.getElementById("answer").innerHTML = "Blue";
+            return true;
+        case "What is the best car brand?":
+            document.getElementById("answer").innerHTML = "BMW";
+            return true;
+        case "What is the best truck brand?":
+            document.getElementById("answer").innerHTML = "Toyota";
+            return true;
+        case "What is the meaning of life?":
+            document.getElementById("answer").innerHTML = "42";
+            return true;
+        default:
+            return false;
     }
 }
+
+const answers = ["This is a sign. A bad sign.",
+"Can you rephrase that?",
+"It is Certain.",
+"Ask three more times",
+"It is decidedly so.",
+"Without a doubt",
+"Yes. Definitely.",
+"You may rely on it",
+"As I see it, yes.",
+"Most Likely.",
+"Don't Blink.",
+"Outlook good.",
+"Yes",
+"Signs point to yes",
+"Reply hazy, try again.",
+"Ask again later",
+"Better not tell you now.",
+"Cannot predict now",
+"Concentrate and ask again.",
+"Don't count on it.",
+"Don't google that.",
+"You're about to drop your phone",
+"Omg are you serious?",
+"Leave town.",
+"My sources say no.",
+"Outlook not so good.",
+"Bigfoot is real",
+"Very doubtful",
+"No.",
+"Send it",
+"FUCKING SEND IT!!!",
+"error"]
